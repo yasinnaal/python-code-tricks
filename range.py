e4edfa05156a0b62a63bfe1f1990a0a909589c40
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-03-30 14:22:17 +0300

    Add files via upload

---
+++ range.py
@@ -0,0 +1,11 @@
+
+#range(start, stop, step_size)
+# To force range function to output all the items, we use the function list()
+
+print(range(10)) # without calling list
+
+print(list(range(10))) #calling list
+
+print(list(range(2, 10))) # start from 2
+
+print(list(range(2, 10, 1))) # start from 2, stop at 10, step 1
