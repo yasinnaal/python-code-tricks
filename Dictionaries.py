20cbb63967cfe5bcb6cd19c1bc32a396080a1026
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-04-01 14:00:51 +0300

    Add files via upload

---
+++ Dictionaries.py
@@ -0,0 +1,8 @@
+
+# Create: Dictionary
+songs = {"Artist":"Celine Dion", "Album":"Let's Talk About Love", "Released": 1997,"Genre":"Pop"}
+
+# Loop Through a Dictionary print Keys + values
+for i in songs:
+    print(i, ":", songs[i])
+
