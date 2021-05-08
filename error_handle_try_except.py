037e71269d72762f8b78fba6b36db0cd8884326c
Yasin Naal <42123587+yasinnaal@users.noreply.github.com>
2021-05-08 14:36:29 +0300

    Add files via upload

---
+++ error_handle_try_except.py
@@ -0,0 +1,11 @@
+
+
+mouse, cat, dog = "small", "medium", "large"
+
+try:
+    print(camel)
+except:
+    print("not difined")
+finally:
+  print("Defined Animals list is:")  
+  print("mouse:" , mouse, "cat:", cat, "dog:", dog)
